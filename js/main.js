43b2b802d2a95cb7a106c8f8b09fe13fcd798e22
Ga1atex <galatexxx@gmail.com>
2021-09-02 20:31:13 +0300

    completed adaptive layout

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 $(function () {
   $('.slider__box').slick({
     prevArrow: '<img class="slider__arrow slider__arrow-left" src="../images/arrow-left.svg" alt="slider arrow-left">',
-    nextArrow: '<img class="slider__arrow slider__arrow-right" src="../images/arrow-right.svg" alt="slider arrow-right">'
+    nextArrow: '<img class="slider__arrow slider__arrow-right" src="../images/arrow-right.svg" alt="slider arrow-right">',
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 896,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
+  });
+
+  $('.menu-btn').on('click', function () {
+    $('.menu__list').toggleClass('active');
   });
 });
