ba966a3a956ae0190fab24e1ecd35b79304fe87a
Ga1atex <galatexxx@gmail.com>
2021-09-15 14:45:29 +0300

    slider arrows fix

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function () {
   $('.slider__box').slick({
-    prevArrow: '<img class="slider__arrow slider__arrow-left" src="../images/arrow-left.svg" alt="slider arrow-left">',
-    nextArrow: '<img class="slider__arrow slider__arrow-right" src="../images/arrow-right.svg" alt="slider arrow-right">',
+    prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/arrow-left.svg" alt="slider arrow-left">',
+    nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/arrow-right.svg" alt="slider arrow-right">',
     autoplay: true,
     autoplaySpeed: 2000,
     responsive: [
